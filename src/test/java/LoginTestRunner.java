359dc9c1ce527a9aa5e8ec4c3ea632579a4f0fa2
SaminCodeMen <codemen.samin@gmail.com>
2022-04-27 11:04:00 +0600

    assignment 2

--- src/test/java/LoginTestRunner.java
+++ src/test/java/LoginTestRunner.java
@@ -2,7 +2,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTestRunner extends Setup {
+
     @Test
+    public void userReg(){
+        driver.get("http://automationpractice.com/");
+        LoginPage loginPage = new LoginPage(driver);
+       int id = (int) (Math.random()*(1000-1)+1);
+       String email = "user"+id+"@test.com";
+        String password = "P@ssword123";
+        loginPage.doReg(email,password);
+    }
+
     public void userLogin() throws InterruptedException {
     LoginPage loginPage = new LoginPage(driver);
     driver.get("http://automationpractice.com/");
